3d1de65e70c8e9c334fd92b9a5548bd39165c7f9
ben knowlson <bknowlson18@VenerableBede.schools.sunderland.gov.uk>
2016-09-22 09:32:39 +0100

    Have fixed all of these problems

--- biggest.py
+++ biggest.py
@@ -26,3 +26,11 @@ def run_tests():
 # Edit this code
 def biggest(a, b, c):
 
+    if a > b and a > c:
+        print(a)
+
+    elif b > a and a > c:
+        print(b)
+
+    else:
+        print(c)
